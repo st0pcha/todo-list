9c8784cc882eedef387215fe57af2b9061f457ef
Stanislav Simahin <stopikytub@gmail.com>
2022-10-28 23:27:48 +0300

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todo, setTodo] = useState([
+		{
+			id: 1,
+			title: 'Make coffee',
+			ended: false
+		}
+	]);
+	const [value, setValue] = useState('');
+	const [ids, setIds] = useState(2);
+
+	const addTodo = () => {
+		setIds(ids + 1);
+
+		setTodo([
+			...todo,
+			{
+				id: ids,
+				title: value,
+				ended: false
+			}
+		]);
+
+		setValue('');
+	};
+
+	const deleteTodo = (id: number) => {
+		setIds(ids + 1);
+		let newTodo = [...todo].filter(item => item.id !== id);
+		setTodo(newTodo);
+	};
+
+	const changeStatus = (id: number) => {
+		let newTodo = [...todo].filter(item => {
+			if (item.id === id) item.ended = !item.ended;
+			return item;
+		});
+
+		setTodo(newTodo);
+	};
+
+	return (
+		<div className="App">
+			<input placeholder="Enter the task" value={value} onChange={e => setValue(e.target.value)} />
+			<button onClick={addTodo}>Add</button>
+			{todo.map(item => (
+				<div key={item.id}>
+					<p onClick={() => changeStatus(item.id)}>{item.ended ? <s>{item.title}</s> : <>{item.title}</>}</p>
+					<button className="btn" onClick={() => deleteTodo(item.id)}>
+						Remove
+					</button>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default App;
